6da24f1d9f38accf612876b01dd319c6f125ea20
karankumar91 <karan.kumar@cloudeq.com>
2023-03-14 12:29:15 +0530

    first commit

--- test/test.py
+++ test/test.py
@@ -1,12 +1,1 @@
-import sys
-
-
-def add(a, b):
-    x = a+b
-    return x
-
-a = int(sys.argv[1])
-b = int(sys.argv[2])
-# c = sys.argv[3]
-
-print(f'sum to {a} and {b} = {add(a, b)}')
+print ("hello world")
